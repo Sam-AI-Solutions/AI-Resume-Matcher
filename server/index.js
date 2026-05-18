b4f850ae51a33fb8f12cf260deaaece98c608464
Sameer Singh <sameersingh1600@gmail.com>
2026-05-17 23:06:53 -0400

    Set up Express backend and integrate Gemini AI resume analysis

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import analyzeRoute from "./routes/analyze.js";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/analyze", analyzeRoute);
 
 app.get("/", (req, res) => {
   res.send("Server running");
